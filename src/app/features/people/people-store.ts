3d1c28c982ea59f3457d2198698e42f33fbd86ce
hein89 <heinrichlosing89@gmail.com>
2026-01-22 17:33:06 +0100

    feat: refactored some code

--- src/app/features/people/people-store.ts
+++ src/app/features/people/people-store.ts
@@ -1,24 +1,57 @@
 import { Injectable, resource } from '@angular/core';
 import { Person } from './person';
+import { SwapiResponse } from '../films/film';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PeopleStore {
 
-  // Lädt alle Personen (Root-Ressource 2)
+// 1. Liste aller Personen aber nur die ersten 10
+/*
   peopleResource = resource({
-    loader: 
-      () => fetch('https://swapi.dev/api/people/')
+    loader: () => 
+      fetch('https://swapi.dev/api/people/')
         .then(res => res.json())
         .then(data => data.results as Person[])
   });
+*/
 
-  // Hilfsmethode, um Details für eine spezifische URL zu laden (wichtig für die Verknüpfung)
-  getPersonByUrl(url: string) {
-  return resource({
-    loader: () => fetch(url).then(res => res.json())
+// Lädt ALLE Personen durch rekursives Abrufen der "next"-Links
+peopleResource = resource({
+    loader: async () => {
+      let allPeople: Person[] = [];
+      let nextUrl: string | null = 'https://swapi.dev/api/people/';
+
+      // Wir laufen so lange durch, bis die API kein 'next' mehr liefert
+      while (nextUrl) {
+        const res = await fetch(nextUrl);
+        const data: SwapiResponse<Person> = await res.json();
+        
+        allPeople = [...allPeople, ...data.results];
+        nextUrl = data.next; // URL der nächsten 10 oder null
+      }
+
+      return allPeople;
+    }
   });
-}
+
+  // 2. NEU: Detail-Resource basierend auf der ID (für die Detail-Route)
+  getPersonResource(idSignal: () => string) {
+    return resource({
+      loader: () => {
+        const currentId = idSignal();
+        return fetch(`https://swapi.dev/api/people/${currentId}/`)
+          .then(res => res.json() as Promise<Person>);
+      }
+    });
+  }
+
+  // 3. Hilfsmethode für direkte URLs (z.B. aus Film-Details)
+  getPersonByUrl(url: string) {
+    return resource({
+      loader: () => fetch(url).then(res => res.json() as Promise<Person>)
+    });
+  }
   
 }
